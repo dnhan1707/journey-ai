50e43a088864adf8973f2588d0a17b67e4196a60
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2024-09-01 15:59:49 -0700

    reverse

--- client-side/src/components/DetailContainer.jsx
+++ client-side/src/components/DetailContainer.jsx
@@ -2,14 +2,8 @@ import ImageContainer from "./ImageContainer";
 import UserInfo from "./UserInfo";
 import DisplayCard from "./DisplayCard";
 import { v4 as uuidv4 } from 'uuid';
-import { useState, useEffect } from "react";
-import LoadingPage from "./LoadingPage.jsx";
 
 function DetailContainer({ location }) {
-    const [fetchDone, setFetchDone] = useState(false);
-    const [fetchImageDone, setFetchImageDone] = useState(false);
-    const [fetchCardDataDone, setFetchCardDataDone] = useState(false);
-
     let planId = localStorage.getItem('currentPlanId');
 
     if (!planId) {
@@ -17,33 +11,22 @@ function DetailContainer({ location }) {
         localStorage.setItem('currentPlanId', planId);
     }
 
-    useEffect(() => {
-        if (fetchImageDone) {
-            setFetchDone(true);
-        }
-    }, [fetchImageDone]); // Trigger only when either of these changes
-
     return (
         <>
-            {fetchDone ? (
-                <div className="detail_container pb-10">
-                    <ImageContainer 
-                        location={location.state.location} 
-                        response={location.state.responseData} 
-                        isImageFetched={setFetchImageDone} 
-                    />
-                    <UserInfo 
-                        likeOption={false} 
-                        isInSavedDestinationPage={false} 
-                        plan_id={planId} 
-                    />
-                    <DisplayCard 
-                        response={location.state.responseData} 
-                    />
-                </div>
-            ) : (
-                <LoadingPage />
-            )}
+            <div className="detail_container pb-10">
+                <ImageContainer 
+                    location={location.state.location} 
+                    response={location.state.responseData} 
+                />
+                <UserInfo 
+                    likeOption={false} 
+                    isInSavedDestinationPage={false} 
+                    plan_id={planId} 
+                />
+                <DisplayCard 
+                    response={location.state.responseData} 
+                />
+            </div>
         </>
     );
 }
