4e371f222ab4b7858bdbf778bb01742afbaf96b9
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2024-09-01 11:41:52 -0700

    update Card

--- client-side/src/components/GenerateDestinationCard.jsx
+++ client-side/src/components/GenerateDestinationCard.jsx
@@ -47,7 +47,7 @@ function GenerateDestinationCard(props) {
     return (
         <div key={props.activityIndex} className="detail pt-6">
             <DetailCard show={openDetail} onClose={cardDetailClicked} placeId={placeId} placeName={placeName} photoURL={photoUrl} placeDetailDataFromDb={props.activity.place_detail}/>
-            <div className="card flex w-full rounded-lg bg-gray-50 p-3">
+            <div className="card flex w-full rounded-lg bg-gray-50 p-3 transition-transform duration-300 ease-in-out hover:scale-105 hover:bg-gray-100">
                 <div className="location_description flex">
                     <div className="description">
                         <button 
