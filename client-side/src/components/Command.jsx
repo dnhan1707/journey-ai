0179215919b794a1bb2b5cc160d5fa4d111c97fe
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2024-11-27 20:34:14 -0800

    small fix for loading page

--- client-side/src/components/Command.jsx
+++ client-side/src/components/Command.jsx
@@ -85,83 +85,87 @@ function Command() {
                 )
             }
 
-            <Snackbar
-                open={openSnackBar}
-                autoHideDuration={5000}
-                onClose={handleCloseSnackBar}
-            >
-                <Alert
-                    onClose={handleCloseSnackBar}
-                    severity="warning"
-                    variant="filled"
-                    sx={{ width: '100%' }}
-                >
-                    {error}
-                </Alert>
-            </Snackbar>
-
-            <div id="command">
-
-                {/* The title */}
-                <div className="text-center landing-margin">
-                    <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Plan your trip in one
-                        click</h1>
-                    <p className="mx-4 mt-6 text-xs leading-4 text-gray-600 sm:text-base">Let us help you generate the perfect plan for the trip
-                        by simply entering your destination, number of travelers, and days.</p>
-                    <img className="landing-icon" src={"notion-icon.png"} alt='journeyAI Icon' />
-                </div>
-
-                
-                <div className="flex items-center justify-center">
-                    {/* Location Input */}
-                    <div className="w-1/2 max-w-lg mx-2">
-                        <LocationInput setLocation={setLocation} />
-                    </div>
-
-                    {/* Customize button */}
-                    <div className="flex-shrink-0 mx-2">
-                        <button className="bg-orange-500 p-3 ring-orange-500 focus:ring-4 focus:outline-none focus:ring-white font-medium rounded-lg text-sm text-center inline-flex items-center dark:bg-orange-500 dark:hover:bg-orange-700 dark:focus:ring-white text-white" 
-                        onClick={handleOpenCustomizeBox}
+            {!isLoading && 
+                <>
+                    <Snackbar
+                        open={openSnackBar}
+                        autoHideDuration={5000}
+                        onClose={handleCloseSnackBar}
+                    >
+                        <Alert
+                            onClose={handleCloseSnackBar}
+                            severity="warning"
+                            variant="filled"
+                            sx={{ width: '100%' }}
                         >
-                            Personalize
-                            <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-                            </svg>
-                        </button>
-                    </div>
-                </div>
+                            {error}
+                        </Alert>
+                    </Snackbar>
 
-                {/* Set number of days and people */}
-                <div className={"trip-options"}>
-                    <div className={"mx-10"}>
-                        <PeopleCount setNumOfPeople={setNumOfPeople} />
-                    </div>
-                    <div className={"mx-10"}>
-                        <DayCount setNumberOfDay={setDay} />
-                    </div>
-                </div>
+                    <div id="command">
 
-                {/* Button to generate plan from input value */}
-                <div className={"flex justify-center"}>
-                    <button
-                        className={"bg-gray-800 hover:bg-orange-500 text-white font-bold py-3 px-4 rounded flex flex-col mb-4"}
-                        onClick={handleSubmit}>Generate Plan
-                    </button>
-                </div>
-
-                {/* Only ask Gemini when the prompt is ready */}
-                {
-                    command && (
-                        <div className={"mx-20 flex justify-center"}>
-                            <GeminiResponse command={command} onDataReceived={handleResponse} setLoading={setLoading} />
+                        {/* The title */}
+                        <div className="text-center landing-margin">
+                            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Plan your trip in one
+                                click</h1>
+                            <p className="mx-4 mt-6 text-xs leading-4 text-gray-600 sm:text-base">Let us help you generate the perfect plan for the trip
+                                by simply entering your destination, number of travelers, and days.</p>
+                            <img className="landing-icon" src={"notion-icon.png"} alt='journeyAI Icon' />
                         </div>
-                    )
-                }
-            </div>
-            {/* <button className="absolute right-40 top-0 h-auto text-gray-600 mx-7 mt-2 hover:text-gray-900 hover:underline py-3 px-2 rounded flex items-center justify-center mb-4 space-x-2">
-                saved plan
-            </button>
-            <SignOutButton></SignOutButton> */}
+
+                        
+                        <div className="flex items-center justify-center">
+                            {/* Location Input */}
+                            <div className="w-1/2 max-w-lg mx-2">
+                                <LocationInput setLocation={setLocation} />
+                            </div>
+
+                            {/* Customize button */}
+                            <div className="flex-shrink-0 mx-2">
+                                <button className="bg-orange-500 p-3 ring-orange-500 focus:ring-4 focus:outline-none focus:ring-white font-medium rounded-lg text-sm text-center inline-flex items-center dark:bg-orange-500 dark:hover:bg-orange-700 dark:focus:ring-white text-white" 
+                                onClick={handleOpenCustomizeBox}
+                                >
+                                    Personalize
+                                    <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+
+                        {/* Set number of days and people */}
+                        <div className={"trip-options"}>
+                            <div className={"mx-10"}>
+                                <PeopleCount setNumOfPeople={setNumOfPeople} />
+                            </div>
+                            <div className={"mx-10"}>
+                                <DayCount setNumberOfDay={setDay} />
+                            </div>
+                        </div>
+
+                        {/* Button to generate plan from input value */}
+                        <div className={"flex justify-center"}>
+                            <button
+                                className={"bg-gray-800 hover:bg-orange-500 text-white font-bold py-3 px-4 rounded flex flex-col mb-4"}
+                                onClick={handleSubmit}>Generate Plan
+                            </button>
+                        </div>
+
+                        {/* Only ask Gemini when the prompt is ready */}
+                        {
+                            command && (
+                                <div className={"mx-20 flex justify-center"}>
+                                    <GeminiResponse command={command} onDataReceived={handleResponse} setLoading={setLoading} />
+                                </div>
+                            )
+                        }
+                    </div>
+                    {/* <button className="absolute right-40 top-0 h-auto text-gray-600 mx-7 mt-2 hover:text-gray-900 hover:underline py-3 px-2 rounded flex items-center justify-center mb-4 space-x-2">
+                        saved plan
+                    </button>
+                    <SignOutButton></SignOutButton> */}
+                </>
+            }
         </>
     );
 }
